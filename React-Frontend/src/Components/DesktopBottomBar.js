3ada94145fbf1e21e6b149e07e5e29a92749fa71
mabroor007 <amabroor007@gmail.com>
2021-04-23 18:08:16 +0500

    fixed some css issues

--- React-Frontend/src/Components/DesktopBottomBar.js
+++ React-Frontend/src/Components/DesktopBottomBar.js
@@ -1,31 +1,32 @@
-import React  from 'react'
-import "../Css/DesktopBottomBar.css"
-import review from '../Components/Images/review-vector.png';
-import upload  from '../Components/Images/upload-vector.png';
-import wallet from '../Components/Images/wallet-vector.png';
+import React from "react";
+import "../Css/DesktopBottomBar.css";
+import review from "../Components/Images/review-vector.png";
+import upload from "../Components/Images/upload-vector.png";
+import wallet from "../Components/Images/wallet-vector.png";
 export default class DesktopBottomBar extends React.Component {
-    render() {
-        return (
-            <div id = "desktop-bottom-bar">
-                <div id = "review-btm" className = "desktop-buttons-btm">
-                     <img src = {review}></img>
-                     <h5>0</h5>
-                </div>
-                <div 
-                    id = "upload-btm" 
-                    className = "desktop-buttons-btm"
-                    onClick = {()=>{window.location.replace("/upload");}}
-                    >
-                     <img src = {upload}></img>  
-                     <h5>{this.props.song_len}</h5>
-                </div>
-               
-                <div id = "wallet-btm" className = "desktop-buttons-btm" >
-                    <img src = {wallet}></img>
-                    <h5>0</h5>
-                </div>
-                
-            </div>
-        )
-    }
-} 
+  render() {
+    return (
+      <div id="desktop-bottom-bar">
+        <div id="review-btm" className="desktop-buttons-btm">
+          <img src={review}></img>
+          <h5>0</h5>
+        </div>
+        <div
+          id="upload-btm"
+          className="desktop-buttons-btm"
+          onClick={() => {
+            window.location.replace("/upload");
+          }}
+        >
+          <img src={upload}></img>
+          <h5>{this.props.song_len}</h5>
+        </div>
+
+        <div id="wallet-btm" className="desktop-buttons-btm">
+          <img src={wallet}></img>
+          <h5>0</h5>
+        </div>
+      </div>
+    );
+  }
+}
